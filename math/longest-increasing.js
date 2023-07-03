a9151920cdb9ebbde34ea17d08b62a1375bb604d
maxizhang <maxizhang@tencent.com>
2023-07-03 17:40:23 +0800

    feat: add3

--- math/longest-increasing.js
+++ math/longest-increasing.js
@@ -20,3 +20,22 @@ var lengthOfLIS = function (nums) {
 
 var arr = [0,1,0,3,2,3]
 lengthOfLIS(arr);
+
+//二分法
+// var lengthOfLIS = function(nums) {
+//     var ans = []; 
+//     for (var i = 0; i < nums.length; i++) {
+//         var left = 0, right = ans.length;
+//         while (left < right) { //二分法
+//             var mid = left + right >>> 1;
+//             if (ans[mid] < nums[i]) left = mid + 1;
+//             else right = mid;
+//         } 
+//         if (right >= ans.length) ans.push(nums[i]); //如果找不到 在ans最后增加一项nums[i]
+//         else ans[right] = nums[i];
+//     }
+//     return ans.length;
+// };
+
+// var arr = [0,1,0,3,2,3]
+// lengthOfLIS(arr);
