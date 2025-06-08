2f787987bfde7e628292166b3103d94ebd2bb138
maxizhang <maxizhang@tencent.com>
2025-06-09 02:07:06 +0800

    feat: 补充发邮件逻辑

--- chrome/taobao/scripts/content.js
+++ chrome/taobao/scripts/content.js
@@ -12,26 +12,51 @@
 
 (function () {
     'use strict';
-     // 间隔时间随机 60-120 秒
-    const refreshInterval = 60 * 1000;
+    // 间隔时间随机 30-90 秒
+    const refreshInterval = 30 * 1000;
     const randomInterval = 60 * 1000;
+    console.log('[maxi] start check liGuang');
+    // location="mailto:sample@fly63.com?subject=test&cc=sample@hotmail.com&subject=主题&body=内容";
+    // <a href="mailto:?to=recipient@example.com&subject=Test&body=Hello">点击发送邮件</a>
+
+    // window.location.href="mailto:236820310@qq.com?subject=理光&subject=tb来货了&body=测试";
+
+    function senEmail(title, body) {
+        
+        window.location.href =
+            'mailto:236820310@qq.com?subject=' + title + '&body=' + body;
+        alert(title)
+    }
 
     function checkAndSendRequest() {
+        const time = refreshInterval + Math.random() * randomInterval;
+        console.log('[maxi] refresh in ', Math.round(time / 1000), 's');
         // 随机间隔刷新一次网页
         const timer = setInterval(() => {
             location.reload();
-        }, refreshInterval + Math.random() * randomInterval);
+        }, time);
 
         // 查找所有class名中含有'Disabled'的元素
         const btn = document
             .querySelector('.jkDKTNzlEM--LeftButtonList--_21fe567')
             ?.querySelector('button')
             ?.querySelector('.jkDKTNzlEM--title--f1020f97');
-        if (!btn || btn.innerText !== '商品已经下架啦~要不要瞧瞧别的~') {
+        if (!btn) {
+            console.log('[maxi] not find btn');
+            alert('找不到按钮，可能是检查失败了');
+            return;
+        }
+
+        if (btn.innerText == '领券购买') {
             clearInterval(timer);
-            alert('来货了！');
+            senEmail('理光有货了', '来货');
+        } else if (btn.innerText !== '商品已经下架啦~要不要瞧瞧别的~') {
+            clearInterval(timer);
+            senEmail('理光上架了', '上架');
         }
     }
     // 页面加载时执行代码
-    window.onload = checkAndSendRequest;
+    window.onload = () => {
+        setTimeout(checkAndSendRequest, 1000);
+    };
 })();
